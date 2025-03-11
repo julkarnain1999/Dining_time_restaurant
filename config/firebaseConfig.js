27f649c2c77d8b42f4d03c966694fb6aea0b0ac7
Md julkarnain <mdjulkarnain043@gmail.com>
2025-03-11 21:58:56 +0530

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQUt4P26DJLt3ltrLFH18H67Va-Wjw8k4",
-  authDomain: "dine-time-app.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: "dine-time-app",
-  storageBucket: "dine-time-app.firebasestorage.app",
-  messagingSenderId: "9422706631",
-  appId: "1:9422706631:web:393eb9796c8582d95dabe2",
-  measurementId: "G-5JTJ0HGD4H",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
